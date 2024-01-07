196d9e7d18f98c78623ab3b40bc909818a06b9a7
Vitalii Umanets <umanetsvitaliy@gmail.com>
2024-01-07 11:59:38 +0000

    tf

--- main.tf
+++ main.tf
@@ -55,36 +55,49 @@ module "gke-workload-identity" {
 }
 
 
-data "google_kms_key_ring" "key_ring" {
-  name     = "sops-flux"
-  location = "global"
-  project  = var.GOOGLE_PROJECT
-}
-
-import {
-  to = google_kms_key_ring.key_ring
-  id = "projects/${var.GOOGLE_PROJECT}/locations/${data.google_kms_key_ring.key_ring.location}/keyRings/${data.google_kms_key_ring.key_ring.name}"
-}
-
-resource "google_kms_key_ring" "key_ring" {
-  count    = data.google_kms_key_ring.key_ring.name != null ? 0 : 1
-  name     = "sops-flux"
-  location = "global"
-  project  = var.GOOGLE_PROJECT
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
-
-# module "kms" {
-#   source             = "github.com/vit-um/terraform-google-kms"
-#   project_id         = var.GOOGLE_PROJECT
-#   keyring            = "sops-flux"
-#   location           = "global"
-#   keys               = ["sops-key-flux"]
-#   prevent_destroy    = false
+# data "google_kms_key_ring" "key_ring" {
+#   name     = "sops-flux"
+#   location = "global"
+#   project  = var.GOOGLE_PROJECT
 # }
+
+# import {
+#   to = google_kms_key_ring.key_ring
+#   id = "projects/${var.GOOGLE_PROJECT}/locations/${data.google_kms_key_ring.key_ring.location}/keyRings/${data.google_kms_key_ring.key_ring.name}"
+# }
+
+# resource "google_kms_key_ring" "key_ring" {
+#   count    = data.google_kms_key_ring.key_ring.name != null ? 0 : 1
+#   name     = "sops-flux"
+#   location = "global"
+#   project  = var.GOOGLE_PROJECT
+
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
+
+module "kms" {
+  source             = "terraform-google-modules/kms/google"
+  version            = "2.2.3"
+  project_id         = var.GOOGLE_PROJECT
+  keyring            = "sops-flux5"
+  location           = "global"
+  keys               = ["sops-key-flux"]
+  prevent_destroy    = false
+}
+
+
+# module "google_kms" {
+#   source          = "terraform-google-modules/kms/google"
+#   version         = "2.2.3"
+#   keyring         = coalesce(data.google_kms_key_ring.key_ring.name, try(google_kms_key_ring.key_ring[0].name, null))
+#   keys            = ["sops-key-flux"]
+#   location        = "global"
+#   project_id      = var.GOOGLE_PROJECT
+#   prevent_destroy = false
+# }
+
 
 resource "null_resource" "cluster_credentials" {
   depends_on = [
@@ -101,7 +114,8 @@ resource "null_resource" "cluster_credentials" {
 
 resource "null_resource" "git_commit" {
   depends_on = [
-    module.flux_bootstrap
+    module.flux_bootstrap,
+    module.kms
   ]
 
   provisioner "local-exec" {
@@ -110,7 +124,8 @@ resource "null_resource" "git_commit" {
         rm -rf ${var.FLUX_GITHUB_REPO}
       fi
       git clone ${module.github_repository.values.http_clone_url}    
-      cp -r demo_app/* ${var.FLUX_GITHUB_REPO}/${var.FLUX_GITHUB_TARGET_PATH}/
+      ./sops -e -gcp-kms ${module.kms.keys.sops-key-flux} --encrypted-regex '^(token)$' secret.yaml > ./demo_app/demo/secret-enc.yaml
+      cp -r demo_app/* ${var.FLUX_GITHUB_REPO}/${var.FLUX_GITHUB_TARGET_PATH}/     
       cd ${var.FLUX_GITHUB_REPO}
       git add .
       git commit -m"Added all manifests"
